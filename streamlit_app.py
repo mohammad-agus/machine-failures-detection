0e390589952311331b74711ba062c019a27a3ff5
Moh. Agus <mohammad_agus@outlook.com>
2025-02-03 01:02:49 +0800

    add stramlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,98 @@
 import streamlit as st
 import requests
+import pandas as pd
 
-
+st.set_page_config("Machine Failure Detection Web App", page_icon="⚙️")
 def main():
-    st.title("Hello World")
+    st.title("Machine Failures Detection")
+    st.image("images/8Rq0.gif")
+    try:
+        with st.sidebar:
+            st.markdown("## Variables:")
+            input_vars = get_response()
+        
+        df_input_vars = pd.DataFrame(
+            data={
+                'variable':input_vars.keys(),
+                'value':input_vars.values()
+            }
+        )
+        st.dataframe(df_input_vars)
+        if st.button("Predict", type='primary'):
+            st.write("Please wait..")
+            response = requests.post(url=st.secrets['API_url'], json=input_vars).json()
+            st.write(response)
+
+    except:
+        pass
 
 
 
 def get_response():
-    
+
+    air_temperature_k = st.slider(
+        label="Air temperature (K)",
+        value=0.0,
+        min_value=290.0,
+        max_value=320.0,
+        step=0.1
+    )
+
+    process_temperature_k = st.slider(
+        label="Process temperature (K)",
+        value=0.0,
+        min_value=290.0,
+        max_value=320.0,
+        step=0.1
+    )
+
+    rotational_speed_rpm = st.slider(
+        label="Rotational speed (RPM)",
+        value=0,
+        min_value=1150,
+        max_value=2900,
+        step=1
+    )
+
+    torque_nm = st.slider(
+        label="Torque (Nm)",
+        value=0.0,
+        min_value=1.0,
+        max_value=80.0,
+        step=0.1
+    )
+
+    tool_wear_min = st.slider(
+        label="Tool wear (minutes)",
+        value=0.0,
+        min_value=0.0,
+        max_value=260.0,
+        step=0.1
+    )
+
+    machine_types = ["L", "M", "H"]
+    type = st.pills(
+            label="Variants of the machine quality",
+            options=machine_types,
+            selection_mode='single'
+    )
+
+    try:
+        input = {
+        'type': type.lower(),
+        'air_temperature_k': air_temperature_k,
+        'process_temperature_k': process_temperature_k,
+        'rotational_speed_rpm': rotational_speed_rpm,
+        'torque_nm': torque_nm,
+        'tool_wear_min': tool_wear_min
+        }
+    except:
+        st.warning("Input all variables!")
+        pass
+
+    return input
 
 
+ 
 if __name__ == "__main__":
     main()
